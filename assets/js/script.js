96714ee1320b88c3295973f2b3e2bd8dc8e347c6
rpharr <rpharr@richmond.edu>
2020-01-06 00:22:36 -0500

    built carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const api_key = "gJ7YOOKcM1WFWMFtihC3dPS3ld1KgSln";
-//https: //api.giphy.com/v1/gifs/search?api_key=gJ7YOOKcM1WFWMFtihC3dPS3ld1KgSln&q=baby yoda&limit=100&offset=0&rating=G&lang=en
 
-let query = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=baby yoda&limit=100&offset=0&rating=G&lang=en`;
+let query = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=baby yoda&limit=75&offset=0&rating=G&lang=en`;
 
-
+var lastIndex = 0;
+var gifcardArray = [];
 
 $.ajax({
     url: query,
@@ -12,18 +12,31 @@ $.ajax({
         alert("some error");
     }
 }).then(function(response) {
-    console.log(response);
-    // $("#json-reponse-ID").text(response.data[0].url);
-
-    console.log("typeof response.data", typeof response.data);
-    console.log("response.data[0].images.fixed_width.url", response.data[0].images.fixed_width.url);
     let i = 0;
     response.data.forEach(element => {
-        console.log(element.images.fixed_width.url);
-        let url = element.images.fixed_width.url;
-        let img = $("<img>").attr("src", url).addClass("byGif");
-        let a = $("<a>").addClass("carousel-item").attr("href", `#${i}`);
-        a.append(img);
-        $("#json-reponse-ID").append(img);
+        let still = element.images.fixed_width_still.url;
+        let animated = element.images.fixed_width.url;
+        let fixedSizeDiv = $("<div>").addClass("gifcard").attr("data-id", i).attr("data-still", still).attr("data-animated", animated);
+        fixedSizeDiv.attr("style", `background-image: url(${still})`);
+        gifcardArray.push(fixedSizeDiv);
+        let a = $("<a>").addClass("carousel-item").attr("href", `#${i++}`);
+        a.append(fixedSizeDiv);
+        $("#carousel-ID").append(a);
+    });
+    // $('.carousel').carousel();
+    $('.carousel').carousel({
+        onCycleTo: function(ele) {
+            // $(ele).childnodes(".gifcard");
+            //stop last to still
+            let last = gifcardArray[lastIndex][0];
+            $(last).attr("style", `background-image: url(${$(last).data("still")})`);
+            //animate current
+            let current = $(ele).children(".gifcard")[0];
+            let animated = $(current).data("animated");
+            let currentIndex = $(current).data("id");
+            $(current).attr("style", `background-image: url(${animated})`);
+            // set last to current
+            lastIndex = currentIndex;
+        }
     });
 });
